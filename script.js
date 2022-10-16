a219ed50b332282690479a714d0c9d76048e0ce5
Edilberto Cantuaria <edilbertocantuaria@users.noreply.github.com>
2022-10-16 16:38:44 -0300

    Botão de pedir: habilitado
    Próxima fase: revisão do pedido!

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+let habilitarBotao = document.querySelectorAll('.borda-selecao').length;
+let verificarPrato = document.querySelector('.prato .borda-selecao');
+let verificarBebida = document.querySelector('.bebida .borda-selecao');
+let verificarSobremesa = document.querySelector('.sobremesa .borda-selecao');
+let selecaoCompleta=0;
+
 function adicionarBorda_Prato(selecionado){
 
     console.log(selecionado);
     
     const selecionadoAnteriormente = document.querySelector(".prato .borda-selecao");
-    
     
     if (selecionadoAnteriormente !== null){
     console.log(selecionadoAnteriormente);
@@ -12,23 +17,24 @@ function adicionarBorda_Prato(selecionado){
     }
    
 	selecionado.classList.add("borda-selecao");
+    verificarPrato = document.querySelector('.prato .borda-selecao');
+    habilitarBotao = document.querySelectorAll('.borda-selecao').length;
    /*document.getElementById("iconeSelecao").style.opacity="";*/
 }
 
 function adicionarBorda_Bebida(selecionado){
-
     console.log(selecionado);
     
     const selecionadoAnteriormente = document.querySelector(".bebida .borda-selecao");
     console.log(selecionadoAnteriormente);
-    
+
     if (selecionadoAnteriormente !== null){
      selecionadoAnteriormente.classList.remove("borda-selecao");
-   /* document.getElementById("iconeSelecao").style.opacity="0";*/
-    }
+      }
    
-	selecionado.classList.add("borda-selecao");
-   /* document.getElementById("iconeSelecao").style.opacity="1";*/
+    selecionado.classList.add("borda-selecao");
+   verificarBebida = document.querySelector('.bebida .borda-selecao');
+   habilitarBotao = document.querySelectorAll('.borda-selecao').length;
 }
 
 function adicionarBorda_Sobremesa(selecionado){
@@ -40,30 +46,31 @@ function adicionarBorda_Sobremesa(selecionado){
     
     if (selecionadoAnteriormente !== null){
      selecionadoAnteriormente.classList.remove("borda-selecao");
-   /* document.getElementById("iconeSelecao").style.opacity="0";*/
-    }
    
+    }
+  
 	selecionado.classList.add("borda-selecao");
-   /* document.getElementById("iconeSelecao").style.opacity="1";*/
+    verificarSobremesa = document.querySelector('.sobremesa .borda-selecao');
+    habilitarBotao = document.querySelectorAll('.borda-selecao').length;
+  
 }
 
-function relizarPedido(apertado){
-const verificarPrato = document.querySelector('.prato .borda-selecao');
-const verificarBebida = document.querySelector('.bebida .borda-selecao');
-const verificarSobremesa = document.querySelector('.sobremesa .borda-selecao');
-let selecaoCompleta=1;
+/*Altera o layout do botão para continuar fazendo o pedido*/ 
+function realizarPedido(apertado){
+    if (habilitarBotao==3){
+        const teste = document.querySelector('.botaoPedido');
+        teste.classList.add('botaoDisponivel');
+        teste.innerHTML="Fechar pedido";
+    }
+}
 
-if (verificarPrato === null || verificarBebida=== null || verificarSobremesa=== null ){
-   /* alert("Selecione o prato!");*/
-selecaoCompleta = 0;
+/*Ao clicar no botão, ele irá verificar se todos os itens foram marcados. Se há algum item faltando, aparece o alerta. Caso contrário, segue para a próxima página*/ 
+function disponibilizarBotao(disponivel){
+    if (verificarPrato === null || verificarBebida=== null || verificarSobremesa=== null ){
+        alert("Vc deve escolher pelo menos um item de cada categoria! Vai ter venda casada sim!!! Quem mandou não aprender cozinhar?! Se não gostou:  \n @drfran  \n @seusdireitos \n Procon: 151");
+    }
+
 } 
 
-if (selecaoCompleta==0){
-    alert("Vc deve escolher pelo menos um item de cada categoria! Vai ter venda casada sim!!!");
-} else {
-    alert("Vc selecionou tudin! Parabéns!!");
-}
-
-}
 
 
